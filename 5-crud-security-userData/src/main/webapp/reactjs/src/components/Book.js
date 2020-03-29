57ce592345b1f5263d45802e46e6846b70f1d033
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-03-29 18:30:05 +0200

    Refactor

--- 5-crud-security-userData/src/main/webapp/reactjs/src/components/Book.js
+++ 5-crud-security-userData/src/main/webapp/reactjs/src/components/Book.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Book extends React.Component {
+export default class Book extends Component {
     render() {
         return (<div className="text-white">Add Book</div>);
     }
 
 
 }
-
-export default Book;
