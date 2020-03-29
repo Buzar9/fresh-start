468ab6810f051b76539bd300ced204e5e1dbb9ec
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-03-29 15:25:46 +0200

    Creat Jumbotron

--- 5-crud-security-userData/src/main/webapp/reactjs/src/App.js
+++ 5-crud-security-userData/src/main/webapp/reactjs/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
-
+import {Container, Row, Col} from 'react-bootstrap';
 import NavigationBar from './components/NavigationBar';
+import Welcome from "./components/Welcome";
 
 function App() {
+    const marginTop = {
+        marginTop:"20px"
+    }
+
   return (
     <div className="App">
         <NavigationBar />
-     <p>
-         Welcome to the Book Shop
-     </p>
+        <Container>
+            <Row>
+                <Col lg={12} style={marginTop}>
+                    <Welcome/>
+                </Col>
+            </Row>
+        </Container>
     </div>
   );
 }
