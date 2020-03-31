5a60ba53335f8388f11c9ee5f93770c4b690806b
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-03-31 09:40:21 +0200

    Add Toast to add User

--- 5-crud-security-userData/src/main/webapp/reactjs/src/components/Book.js
+++ 5-crud-security-userData/src/main/webapp/reactjs/src/components/Book.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Card, Form, Button, Col} from "react-bootstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSave, faPlusSquare, faUndo} from "@fortawesome/free-solid-svg-icons";
-
+import MyToast from "./MyToast";
 import axios from 'axios';
 
 export default class Book extends Component {
@@ -10,6 +10,7 @@ export default class Book extends Component {
     constructor(props) {
         super(props)
         this.state = this.initialState;
+        this.state.show = false;
         this.bookChange = this.bookChange.bind(this);
         this.submitBook = this.submitBook.bind(this);
     }
@@ -36,10 +37,14 @@ export default class Book extends Component {
         axios.post("http://localhost:8080/users", book)
             .then(response => {
                 if(response.data != null) {
-                    this.setState(this.initialState);
-                    alert("Book Saved Successfully")
+                    this.setState({'show':true});
+                    setTimeout(() => this.setState({'show':false}), 3000);
+                } else {
+                    this.setState({'show':false});
                 }
             });
+
+        this.setState(this.initialState);
     }
 
     bookChange= event => {
@@ -52,57 +57,64 @@ export default class Book extends Component {
         const {username, password, enabled, roles} = this.state;
 
         return (
-            <Card className={"border border-dark bg-dark text-white"}>
-                <Card.Header><FontAwesomeIcon icon={faPlusSquare}/> Book List</Card.Header>
-                <Form onReset={this.resetBook} onSubmit={this.submitBook} id="bookFormId">
-                    <Card.Body>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="formGridUsername">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control required autoComplete="off"
-                                    type="text" name="username"
-                                    value={username} onChange={this.bookChange}
-                                    className={"bg-dark text-white"}
-                                    placeholder="Enter Username" />
-                            </Form.Group>
-                            <Form.Group as={Col} controlId="formGridPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control required autoComplete="off"
-                                    type="text" name="password"
-                                    value={password} onChange={this.bookChange}
-                                    className={"bg-dark text-white"}
-                                    placeholder="Enter Password" />
-                            </Form.Group>
-                        </Form.Row>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="formGridEnabled">
-                                <Form.Label>Enabled</Form.Label>
-                                <Form.Control required autoComplete="off"
-                                    type="text" name="enabled"
-                                    value={enabled} onChange={this.bookChange}
-                                    className={"bg-dark text-white"}
-                                    placeholder="Is Enabled?" />
-                            </Form.Group>
-                            <Form.Group as={Col} controlId="formGridRoles">
-                                <Form.Label>Roles</Form.Label>
-                                <Form.Control required autoComplete="off"
-                                    type="text" name="roles"
-                                    value={roles} onChange={this.bookChange}
-                                    className={"bg-dark text-white"}
-                                    placeholder="Enter Roles" />
-                            </Form.Group>
-                        </Form.Row>
-                    </Card.Body>
-                <Card.Footer style={{"textAlign":"right"}}>
-                    <Button size="sm" variant="success" type="submit">
-                        <FontAwesomeIcon icon={faSave}/> Submit
-                    </Button>{" "}
-                    <Button size="sm" variant="info" type="reset">
-                        <FontAwesomeIcon icon={faUndo} /> Reset
-                    </Button>
-                </Card.Footer>
-            </Form>
-            </Card>
+            <div>
+                <div style={{'display':this.state.show ? 'block' : 'none'}}>
+                    <MyToast children={{show:this.state.show, message:'User Saved Successfully.'}}/>
+                </div>
+                <Card className={"border border-dark bg-dark text-white"}>
+                    <Card.Header><FontAwesomeIcon icon={faPlusSquare}/> Book List</Card.Header>
+                    <Form onReset={this.resetBook} onSubmit={this.submitBook} id="bookFormId">
+                        <Card.Body>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridUsername">
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="username"
+                                                  value={username} onChange={this.bookChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Enter Username" />
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="password"
+                                                  value={password} onChange={this.bookChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Enter Password" />
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridEnabled">
+                                    <Form.Label>Enabled</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="enabled"
+                                                  value={enabled} onChange={this.bookChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Is Enabled?" />
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridRoles">
+                                    <Form.Label>Roles</Form.Label>
+                                    <Form.Control required autoComplete="off"
+                                                  type="text" name="roles"
+                                                  value={roles} onChange={this.bookChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Enter Roles" />
+                                </Form.Group>
+                            </Form.Row>
+                        </Card.Body>
+                        <Card.Footer style={{"textAlign":"right"}}>
+                            <Button size="sm" variant="success" type="submit">
+                                <FontAwesomeIcon icon={faSave}/> Submit
+                            </Button>{" "}
+                            <Button size="sm" variant="info" type="reset">
+                                <FontAwesomeIcon icon={faUndo} /> Reset
+                            </Button>
+                        </Card.Footer>
+                    </Form>
+                </Card>
+            </div>
+
+
 
         );
     }
