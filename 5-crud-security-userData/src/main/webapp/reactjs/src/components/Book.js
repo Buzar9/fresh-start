ec9215b8daea4fb973bc86e2e484bfba5dfaf9b2
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-03-31 08:53:20 +0200

    Add reset button and refactor ups

--- 5-crud-security-userData/src/main/webapp/reactjs/src/components/Book.js
+++ 5-crud-security-userData/src/main/webapp/reactjs/src/components/Book.js
@@ -1,50 +1,58 @@
 import React, { Component } from 'react';
 import {Card, Form, Button, Col} from "react-bootstrap";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faSave, faPlusSquare} from "@fortawesome/free-solid-svg-icons";
+import {faSave, faPlusSquare, faUndo} from "@fortawesome/free-solid-svg-icons";
 
 export default class Book extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {username:"", password:"", enabled:"", roles:""};
+        this.state = this.initialState;
         this.bookChange = this.bookChange.bind(this);
         this.submitBook = this.submitBook.bind(this);
     }
 
-    submitBook(event) {
+    initialState = {
+        username:"", password:"", enabled:"", roles:""
+    };
+
+    resetBook = () => {
+        this.setState(() => this.initialState);
+    }
+
+    submitBook = event => {
         alert("Username: " + this.state.username+ "Password: " +this.state.password);
         event.preventDefault();
     }
 
-    bookChange(event) {
+    bookChange= event => {
         this.setState({
             [event.target.name]:event.target.value
         });
     }
 
     render() {
+        const {username, password, enabled, roles} = this.state;
+
         return (
             <Card className={"border border-dark bg-dark text-white"}>
                 <Card.Header><FontAwesomeIcon icon={faPlusSquare}/> Book List</Card.Header>
-                <Form onSubmit={this.submitBook} id="bookFormId">
+                <Form onReset={this.resetBook} onSubmit={this.submitBook} id="bookFormId">
                     <Card.Body>
                         <Form.Row>
                             <Form.Group as={Col} controlId="formGridUsername">
                                 <Form.Label>Username</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete="off"
                                     type="text" name="username"
-                                    value={this.state.username}
-                                    onChange={this.bookChange}
+                                    value={username} onChange={this.bookChange}
                                     className={"bg-dark text-white"}
                                     placeholder="Enter Username" />
                             </Form.Group>
                             <Form.Group as={Col} controlId="formGridPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete="off"
                                     type="text" name="password"
-                                    value={this.state.password}
-                                    onChange={this.bookChange}
+                                    value={password} onChange={this.bookChange}
                                     className={"bg-dark text-white"}
                                     placeholder="Enter Password" />
                             </Form.Group>
@@ -52,19 +60,17 @@ export default class Book extends Component {
                         <Form.Row>
                             <Form.Group as={Col} controlId="formGridEnabled">
                                 <Form.Label>Enabled</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete="off"
                                     type="text" name="enabled"
-                                    value={this.state.enabled}
-                                    onChange={this.bookChange}
+                                    value={enabled} onChange={this.bookChange}
                                     className={"bg-dark text-white"}
                                     placeholder="Is Enabled?" />
                             </Form.Group>
                             <Form.Group as={Col} controlId="formGridRoles">
                                 <Form.Label>Roles</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete="off"
                                     type="text" name="roles"
-                                    value={this.state.roles}
-                                    onChange={this.bookChange}
+                                    value={roles} onChange={this.bookChange}
                                     className={"bg-dark text-white"}
                                     placeholder="Enter Roles" />
                             </Form.Group>
@@ -73,6 +79,9 @@ export default class Book extends Component {
                 <Card.Footer style={{"textAlign":"right"}}>
                     <Button size="sm" variant="success" type="submit">
                         <FontAwesomeIcon icon={faSave}/> Submit
+                    </Button>{" "}
+                    <Button size="sm" variant="info" type="reset">
+                        <FontAwesomeIcon icon={faUndo} /> Reset
                     </Button>
                 </Card.Footer>
             </Form>
